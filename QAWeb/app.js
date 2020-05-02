7e69cb88f8c3b8a609d08417f69a3f5c6e821660
Sir-Ignis <masterdaniel.dutra@googlemail.com>
2020-05-03 00:10:53 +0100

    Removed merge garbage

--- QAWeb/app.js
+++ QAWeb/app.js
@@ -15,22 +15,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const LOGS = "logs/log-file.txt";
 const DIR = 'data/'
 const pythonProcess = spawn('python',["scripts/extractWikiData.py"]);
-<<<<<<< HEAD
 
 
 app.get('/', async function (req, res) {
     var data = {reftext:null,q:null,answer:null,sysmsgs:null,logs:null};
-=======
+
 var files = fs.readdirSync(DIR);
 
 app.get('/', async function (req, res) {
     var data = {reftext:null,q:null,answer:null,sysmsgs:null,logs:null,files:files};
-<<<<<<< HEAD
->>>>>>> QAWebTest/master
-||||||| merged common ancestors
-=======
+
     var form = {fileName: req.body.fileName};
->>>>>>> QAWebTest/master
+
     console.log("Rendering: index page");
     res.render('index',{data:data,form:form});
 });
@@ -43,40 +39,38 @@ app.post('/myform', async function(req, res, next){
       logs = fs.readFileSync(LOGS,"utf8");
       logs = logs.split(/\r?\n/);
       var answer = await qa.main(fileName, question);
-<<<<<<< HEAD
-<<<<<<< HEAD
+
       var data = {reftext:qa.fetchText(fileName),q:question,answer:await Object.values(answer)[0],sysmsgs:null,logs:logs}
-=======
+
       var data = {reftext:qa.fetchText(fileName),q:question,answer:await Object.values(answer)[0],sysmsgs:null,logs:logs,files:files}
->>>>>>> QAWebTest/master
+
       res.render('index', {data:data});
-||||||| merged common ancestors
+
       var data = {reftext:qa.fetchText(fileName),q:question,answer:await Object.values(answer)[0],sysmsgs:null,logs:logs,files:files}
       res.render('index', {data:data});
-=======
+
       var reftext = JSON.stringify(qa.fetchText(fileName));
       reftext = reftext.replace(/(\\n)+/g, '<br /><br />');
       var data = {reftext:reftext,q:question,answer:await Object.values(answer)[0],sysmsgs:null,logs:logs,files:files}
       var form = {fileName: fileName};
       res.render('index', {data:data,form:form});
->>>>>>> QAWebTest/master
+
     } else {
       logs = fs.readFileSync(LOGS,"utf8");
       logs = logs.split(/\r?\n/);
       var msg = "ERROR: file '"+fileName+"' does not exist!"
       console.log(msg);
-<<<<<<< HEAD
+
       var data = {reftext:null,q:null,answer:null,sysmsgs:msg,logs:logs}
-=======
+
       var data = {reftext:null,q:null,answer:null,sysmsgs:msg,logs:logs,files:files}
-<<<<<<< HEAD
->>>>>>> QAWebTest/master
+
       res.render('index', {data:data});
-||||||| merged common ancestors
+
       res.render('index', {data:data});
-=======
+
       res.render('index', {data:data,form:form});
->>>>>>> QAWebTest/master
+
     }
 });
 
